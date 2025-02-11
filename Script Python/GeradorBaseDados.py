10bb207f6e7038fc7f9939911c3ad27a82767302
JoaoVazMello <joaovazmello@gmail.com>
2025-02-11 13:51:17 -0300

    Atualização no codigo: GeradorBaseDados.py / Modificaçao na base de dados

--- Script Python/GeradorBaseDados.py
+++ Script Python/GeradorBaseDados.py
@@ -1,20 +1,27 @@
 import pandas as pand
 
-# import do csv para dentro do codigo
+# Import do csv para dentro do codigo.
+# Coloque dentro das () os path de onde esta sua base.
 df = pand.read_csv("games_may2024_full.csv")
 
-# lista de colunas para manter
-# appID / name / release_date / required_age / price / genres
+# Lista de colunas para manter
+# appID / name / release_date / required_age / price / genres / short_description
 
-# caso queira colocar mais colunas basta
+# Caso queira colocar mais colunas basta
+colunas_necessarias = ["AppID", "name", "release_date", "required_age", "price", "genres", "short_description"]
 
-colunas_necessarias = ["AppID", "name", "release_date", "required_age", "price", "genres"]
-
-# transforma o nome das colunas em uma lista para ser percorrida
+# Transforma o nome das colunas em uma lista para ser percorrida
 nomes_colunas = df.columns.tolist()
 
+# Limita uma coluna a um tamanho maximo de 250 caracteres
+# Caso queira trocar basta mudar o nome de dos [] e a quantidade de caracteres
+df["short_description"] = df["short_description"].astype(str).apply(lambda x: x[:250])
+
+# For-each que percorre todo a lista de colunas necessarias testando se aquela coluna esta dentro da lista senao estiver ela e apagada
 for coluna in df.columns:
   if coluna not in colunas_necessarias:
     df = df.drop(coluna, axis = 1)
 
-df.to_csv("BaseDeDados.csv", index = True, sep = ";", encoding = "utf-8")
+# Salva a nova base de dados sem index
+# Caso queira que ela crie um index altomatico basta trocar o index = False para index = True 
+df.to_csv("BaseDeDados.csv", index = False, sep = ";", encoding = "utf-8")
